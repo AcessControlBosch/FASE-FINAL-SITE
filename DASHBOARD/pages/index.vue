b856b2b51c244978bc8b1a641bb5bc5120301683
Mazzaro Guilherme (CaP/ETS) <mgu4ca@bosch.com>
2022-11-08 16:44:23 -0300

    dashs

--- DASHBOARD/pages/index.vue
+++ DASHBOARD/pages/index.vue
@@ -6,7 +6,35 @@
 
     <div class="content">
 
-      <div class="options_bar"></div>
+      <div class="options_bar">
+        
+        <div class="logo-container">
+
+          <img src="../static/logo.svg" />
+
+        </div>
+
+        <div class="menu-container">
+
+          <button class="btn-machine" v-on:click="containerGeral">
+            <p class="text-machine-item">Análise Geral</p>
+          </button>
+
+          <button class="btn-machine-title">
+            <p class="text-machine-item">Máquinas</p>
+          </button>
+          
+          <div v-for="(machine, id) in machines" :key="id">
+
+            <button class="btn-machine-item" v-on:click="mudation(machine)">
+              <p class="text-machine-item">{{machine}}</p>
+            </button>
+                        
+          </div>
+
+        </div>
+
+      </div>
       
       <div class="view_data">
 
@@ -47,17 +75,17 @@
               </div>
 
               <div class="card-body">
-                <p class="text-body">{{this.machines.length}}</p>
+                <p class="text-body">{{this.qtdMachines.length}}</p>
               </div>
             </div>
 
             <div class="card red">
               <div class="card-title">
-                <p class="title">Máquinas quebradas</p>
+                <p class="title">Máquinas em manutenção</p>
               </div>
 
               <div class="card-body">
-                <p class="text-body">0</p>
+                <p class="text-body">{{this.qtdManutencao}}</p>
               </div>
             </div>
 
@@ -102,10 +130,12 @@
               <div class="card-grapich">
 
                 <div class="card-grapich-content-title">
-                    <p class="card-grapich-title-text">Vamo ver o que sai daqui</p>
+                    <p class="card-grapich-title-text">Comparação entre liberações e manutenções</p>
                 </div>
 
                 <div class="container-grapich">
+
+                    <Chart type="bar" :data="lineChartRelease" :options="horizontalOptions"/>
 
                 </div>
 
@@ -121,9 +151,53 @@
 
           </div>
 
+          <div v-if="geral === false" class="only-machines">
+
+            <div class="title-machine-name">
+
+              <p class="title-machine-text">Tempo de utilização da máquina por mês</p>
+
+            </div>
+
+            <div class="grapich">
+
+              <Chart type="bar" :data="lineHourPerMonth"/>
+
+            </div>
+
+            <div class="title-machine-name">
+
+              <p class="title-machine-text">Dados gerais</p>
+
+            </div>
+
+            <div class="size-tables">
+
+              <div class="size_one">
+
+                <div class="title-table">
+                  <p class="title-table-text">Quantidade de manutenções por mês</p>
+                </div>
+              
+              </div>
+
+              <div class="size_two">
+
+                <div class="title-table">
+                  <p class="title-table-text">Quantidade de liberações por mês</p>
+                </div>
+              
+              </div>
+            
+            </div>
           
+          </div>
+        
+        </div>
 
         </div>
+
+        
       
       </div>
 
@@ -157,6 +231,7 @@ export default {
       //Dados para análise das máquinas
       dataMachines: [],
       machines: [],
+      qtdMachines: [],
       manuntecoes: [],
 
       //Dados para a página
@@ -165,6 +240,8 @@ export default {
       day: '',
       month: '',
       year: '',
+      qtdManutencao: '',
+      nameMachine: '',
       
       //PIECHART -  Utilização por classe
       pieChartReleases: [],
@@ -173,7 +250,16 @@ export default {
 
       //BARCHART - Utilização por mês
       barChartMonth: [],
-      releaseMonth: []
+      releaseMonth: [],
+
+      //LINECHART - Comparação entre liberações e manutenções
+      lineChartRelease: [],
+      horizontalOptions: {
+          indexAxis: 'y',
+      },
+
+      //BARCHART - Utilização em horas por mês
+      lineHourPerMonth: [],
       
 		}
 
@@ -201,6 +287,16 @@ export default {
 
       console.log(error)
 
+    }),
+
+    this.$axios.get(this.$store.state.BASE_URL + "/machines/").then((response) => {
+      
+      this.qtdMachines = response.data;
+
+    }).catch((error) => {
+
+      console.log(error)
+
     })
 
   },
@@ -212,10 +308,24 @@ export default {
       const date = new Date();
       let increment = 0;
       let verify = false;
+      let countQtd = 0;
 
       this.date = date.getDate();
       this.month = date.getMonth() + 1;
       this.year =  date.getFullYear();
+
+      //--------------------------------------------------
+      //QUANTIDADE DE MANUTENÇÕES
+      //--------------------------------------------------
+      for(increment; increment < this.qtdMachines.length; increment++){
+
+        if(this.qtdMachines[increment].statusMaint === true){
+          countQtd = countQtd + 1
+        }
+
+      }
+      
+      this.qtdManutencao = countQtd;
 
       //--------------------------------------------------
       //PIECHART - Utilização por classe
@@ -276,7 +386,7 @@ export default {
         labels: this.class,
         datasets: [{
           data: this.classQuant,
-          backgroundColor: this.$store.state.cores,
+          backgroundColor: this.$store.state.coresIniciais,
         }]
       }
 
@@ -339,7 +449,7 @@ export default {
     
       }
       
-      //PReparação dos dados para serem redenizados
+      //Preparação dos dados para serem redenizados
       increment = 0;
       let dataSetMonth = []
       
@@ -348,7 +458,7 @@ export default {
         dataSetMonth.push(
           {
             label: this.machines[increment],
-            backgroundColor: this.$store.state.cores[increment],
+            backgroundColor: this.$store.state.coresIniciais[increment],
             data: [this.releaseMonth[increment]]
           }
 
@@ -362,6 +472,167 @@ export default {
         labels: datasetmonthlabel,
         datasets: dataSetMonth
       }
+
+      //--------------------------------------------------
+      //LINECHART - Diferença entre Liberação x Manuenções
+      //--------------------------------------------------
+      increment = 0
+      let datasetrelease = []
+
+      let labelRelease = ['Liberações',  'Manutenções']
+      let dataRelease = [this.dataMachines.length, this.manuntecoes.length]
+
+      for(increment; increment < 2; increment++){
+
+        datasetrelease.push(
+          {
+            label: labelRelease[increment],
+            backgroundColor: this.$store.state.coresIniciais[increment],
+            data: [dataRelease[increment]]
+          }
+        )
+      }
+
+      let datasetreleaselabel = ['']
+
+      this.lineChartRelease = {
+        labels: datasetreleaselabel,
+        datasets: datasetrelease
+      }
+
+    },
+
+    containerGeral: function(){
+      this.geral = true;
+      this.title = "Análise geral das máquinas"
+    },
+
+    mudation: function(nameMachine){
+
+      this.geral = false;
+      this.title = "Análise da máquina: " + nameMachine
+      this.nameMachine = nameMachine
+
+      let increment = 0;
+      let month = 0;
+      let monthTime = [0,0,0,0,0,0,0,0,0,0,0,0]
+    
+      //--------------------------------------------------
+      //TEMPO DE UTILIZAÇÃO - Verificar quanto tempo essa máquina foi utilizada por mês
+      //--------------------------------------------------
+      for(increment; increment < this.dataMachines.length; increment++){
+
+        if(this.dataMachines[increment].idMachineFK.name ===  nameMachine.toString()){
+
+          let data = this.dataMachines[increment].date.split("-").map(e => parseInt(e))
+
+          for(month; month < 13; month++){
+
+            if(data[1] === month){
+
+              let horaInicial = this.dataMachines[increment].InitialHour;
+              let horaFinal = this.dataMachines[increment].FinishHour;
+
+              let resultado = this.calcularDiferencaHora(horaInicial, horaFinal)
+
+              let mes = monthTime[month]
+
+              monthTime[month] = mes + resultado
+
+            }
+
+          }
+
+        }
+
+      }
+
+      let labelMonth = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+      let dataSetMonth = [];
+      increment = 0;
+
+      for(increment; increment < 12; increment++){
+
+        dataSetMonth.push(
+          {
+            label: labelMonth[increment],
+            backgroundColor: this.$store.state.coresIniciais[increment],
+            data: [monthTime[increment]]
+          }
+        )
+
+      }
+
+      let datasetmonthlabel = ['']
+
+      this.lineHourPerMonth = {
+        labels: datasetmonthlabel,
+        datasets: dataSetMonth
+      }
+
+      //--------------------------------------------------
+      //QUANTIDADE DE MANUTENÇÕES
+      //--------------------------------------------------
+      month = 0;
+      increment = 0
+
+      let qtdMaintence = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+      for(increment; increment < 12; increment++){
+
+        for(month; month < this.observations.length; month++){
+
+          
+
+        }
+
+        
+      }
+
+      
+
+
+    },
+
+    calcularDiferencaHora: function(horaInicialRelease, horaFinalRelease){
+
+      let [horaInicial, minutoInicial, segundoInicial] = horaInicialRelease.split(':').map(hI => parseInt(hI))
+      let [horaFinal, minutoFinal, segundoFinal] = horaFinalRelease.split(':').map(hF => parseInt(hF))
+
+      let somaInicial = 0;
+      let somaFinal = 0;
+
+      //CONVERSÃO PARA HORA - INICIAL
+      if(minutoInicial !== 0){
+        minutoInicial = minutoInicial / 60
+      }
+
+      if(segundoInicial !== 0){
+        segundoInicial = segundoInicial / 3600
+      }
+
+      somaInicial = horaInicial + minutoInicial + segundoInicial
+
+      //CONVERSÃO PARA HORA - FINAL
+      if(minutoFinal !== 0){
+        minutoFinal = minutoFinal / 60
+      }
+
+      if(segundoFinal !== 0){
+        segundoFinal = segundoFinal / 3600
+      }
+
+      somaFinal = horaFinal + minutoFinal + segundoFinal
+
+      let diferenca = 0
+      
+      if(somaInicial > somaFinal){
+        diferenca = somaInicial - somaFinal
+      } else {
+        diferenca = somaFinal - somaInicial
+      }
+
+      return diferenca;
 
     }
 
--- DASHBOARD/store/index.js
+++ DASHBOARD/store/index.js
@@ -2,14 +2,29 @@ export const state = () => ({
 
     BASE_URL: "http://localhost:8000",
 
-    cores: [
+    coresIniciais: [
        //Cores primários - fortes
-       "#FF2124", //red
-       "#001222", //blue
-       "#FFCF00", //yellow"
        "#219557", //green
+       "#001222", //blue
+       "#FF2124", //red
+       "#FFCF00", //yellow"
+       "#B12EA9", //purple
+       "#006EAD", //lightblue
+       "#147671", //turquiose
+       "#007A42", //darkgreen
+       "#DEB300", //yellowdark
+       "#D50005", //red
+       "#003E64", //blue
+       "#00884A"//
        
-        
+    ],
+
+    coresGradient:[
+        "#ED0007", //red
+        "#9E2896", //purple
+        "#007BC0", //blue
+        "#18837E", //turquoise
+        "#00884A", //green
     ]
 
 
